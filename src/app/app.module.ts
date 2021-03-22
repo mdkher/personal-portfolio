4b65147e51425f929790b4eefaab107aca6e7976
Manan Kher <mdkher@gmail.com>
2021-03-22 08:52:57 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingPageComponent } from './loading-page/loading-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
